4f50b3914bc137e212ab98e9995391ec30098cd8
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-02-27 14:01:11 -0400

    Update return to return a list of of all of the objects

--- functions/interests/allInterests.js
+++ functions/interests/allInterests.js
@@ -2,21 +2,17 @@ const { db } = require('../admin_init')
 
 /* 
 Method returns all interests and their attributes:
-{ 
-    "Salsa.1":{
-        "subjectName": "Salsa",
-        "img": "",
-        "id": 1
-    }
-}
+[
+    {id: 1, subjectName: 'Programming'}
+]
 */
 
 exports.allInterests = async (req, res) => {
     try {
         const usersSnapshot = await db.collection('interests').get()
-        interests = {}
+        interests = []
         usersSnapshot.docs.map((doc) => {
-            interests[doc.data().subjectName + '.' + doc.data().id] = doc.data()
+            interests.push(doc.data())
         })
         return res.status(200).json(interests)
     } catch (err) {
