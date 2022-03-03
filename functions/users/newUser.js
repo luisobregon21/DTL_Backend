7dc2dee258a4b54c2653798d6ae187d0958b014d
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-03-03 15:36:49 -0400

    send email verification when sign up

--- functions/users/newUser.js
+++ functions/users/newUser.js
@@ -2,6 +2,7 @@ const { Timestamp } = require('firebase-admin/firestore')
 const { db } = require('../admin_init')
 const { createUserWithEmailAndPassword } = require('firebase/auth')
 const { auth, firebaseConfig } = require('../app_init')
+const { sendEmailVerification } = require('firebase/auth')
 
 config = firebaseConfig
 
@@ -36,6 +37,7 @@ exports.newUser = async (req, res) => {
 
         // See the UserRecord reference doc for the contents of userRecord.
         console.log('Successfully created new user:', user.uid)
+
         //creating new user in users table that matches newly created user in firebase auth
         const newUser = {
             id: user.uid,
@@ -52,6 +54,7 @@ exports.newUser = async (req, res) => {
 
         await db.collection('users').doc(user.uid).set(newUser)
         const token = await user.getIdToken()
+        sendEmailVerification(user)
 
         return res.status(201).json({
             ...newUser,
